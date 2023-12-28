5fa03fc08946bd262e22733c23a2e74e784446cb
kunal1325 <kunalsharma25698@gmail.com>
2023-12-29 00:54:43 +0530

    Api Structure ready

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -15,20 +15,42 @@ class HomeController extends GetxController {
 
   FaceScanRequirementModel? faceScanRequirementModel;
 
+  var configId = "".obs;
+  var token = "".obs;
+  var refreshToken = "".obs;
+  var encryptProfileData = "".obs;
+
   void incrementCounter() {
-    // Utils.showSnackBarFun(Get.context, Strings.alreadyTakenError);
-    getConfigIdApi().then((value) {
-        counter.value++;
-        if (kDebugMode) {
-          print("For Testing ");
-        }
-    });
+    getConfigIdApi();
+    counter.value++;
+    if (kDebugMode) {
+      print("For Testing ");
+    }
   }
 
   Future<void> getConfigIdApi() async {
     try{
       isDataLoading(true);
+      http.Response response = await http.get(
+        Uri.tryParse('http://assessment.hibiscushealth.com/api/configId')!,
+      );
+      if(response.statusCode == 200){
+        var result = jsonDecode(response.body);
+        faceScanRequirementModel =  FaceScanRequirementModel.fromJson(result);
+        print(' getConfigIdApi =============>>>>');
+        configId.value = faceScanRequirementModel?.configId ?? "";
+      }else{
+        Utils.showSnackBarFun(Get.context, Strings.somethingWentWrong);
+      }
+    }catch(e){
+      print('Error while getting data is $e');
+    }finally{
+      getTokenApi();
+    }
+  }
 
+  Future<void> getTokenApi() async {
+    try{
       http.Request req = http.Request("Post", Uri.parse("http://assessment.hibiscushealth.com/api/token"))..followRedirects = false;
       http.Client baseClient = http.Client();
       http.StreamedResponse response1 = await baseClient.send(req);
@@ -40,8 +62,45 @@ class HomeController extends GetxController {
       if(response.statusCode == 200){
         var result = jsonDecode(response.body);
         faceScanRequirementModel =  FaceScanRequirementModel.fromJson(result);
-        print(' FaceScanRequirementModel =============>>>>');
-        print('${faceScanRequirementModel?.token}');
+        print(' getTokenApi =============>>>>');
+        token.value = faceScanRequirementModel?.token ?? "";
+        refreshToken.value = faceScanRequirementModel?.refreshToken ?? "";
+      }else{
+        print('response statusCode ${response.statusCode}');
+      }
+    }catch(e){
+      print('Error while getting data is $e');
+    }finally{
+      getEncryptProfileApi();
+    }
+  }
+
+  Future<void> getEncryptProfileApi() async {
+    try{
+      http.Request req = http.Request("Post", Uri.parse("http://assessment.hibiscushealth.com/api/encrypt-profile"))..followRedirects = false;
+      http.Client baseClient = http.Client();
+      http.StreamedResponse response1 = await baseClient.send(req);
+      Uri redirectUri = Uri.parse(response1.headers['location']!);
+
+      var response = await http.post(redirectUri,
+          headers: {'Content-Type': "application/json; charset=utf-8"},
+        body: json.encode(DataCollectionModel(
+          identifier: "Kunal.hh@gmail.com",
+          age: 26,
+          height: 198,
+          weight: 189,
+          gender: "male",
+          smoking: "1",
+          bloodPressureMedication: "1",
+          diabetes: false
+        ))
+      );
+
+      if(response.statusCode == 200){
+        var result = jsonDecode(response.body);
+        faceScanRequirementModel =  FaceScanRequirementModel.fromJson(result);
+        print(' getEncryptProfileApi =============>>>>');
+        encryptProfileData.value = faceScanRequirementModel?.encryptedProfile ?? "";
       }else{
         print('response statusCode ${response.statusCode}');
       }
@@ -49,7 +108,17 @@ class HomeController extends GetxController {
       print('Error while getting data is $e');
     }finally{
       isDataLoading(false);
+      faceScanRequirementModel = FaceScanRequirementModel(
+        configId: configId.value,
+        token: token.value,
+        refreshToken: refreshToken.value,
+        encryptedProfile: encryptProfileData.value
+      );
+      print(' FaceScanRequirementModel =============>>>>\n');
+      print(faceScanRequirementModel?.toJson());
     }
   }
+
+
 
 }
